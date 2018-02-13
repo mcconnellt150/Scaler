0f260d8714cccfdbbbbae1265cf7f896a9edf40e
mcconnellt150 <35299162+mcconnellt150@users.noreply.github.com>
2018-02-13 13:00:19 +0000

    Create Install-WebServerPrerequisites.ps1

---
+++ Install-WebServerPrerequisites.ps1
@@ -0,0 +1,20 @@
+param (
+    [parameter()]
+    [ValidateNotNullOrEmpty()]
+    [String]$NewRelicLicenseKey
+)
+
+Write-Host "Installing Chocolatey..."
+.\install.ps1
+
+Write-Host "Installing Octopus Deploy DSC Extension..."
+.\Install-OctopusDSC.ps1
+
+Write-Host "Installing Roles..."
+Import-Csv .\Roles.csv | foreach{Add-WindowsFeature $_.name  }
+
+Write-Host "Installing webdeploy..."
+choco install webdeploy /y
+
+Write-Host "installing urlrewrite..."
+choco install urlrewrite /y
